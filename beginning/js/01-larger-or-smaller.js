54579c620c93471e2f3f83f9e86f5144cbef1a58
Dil Somb <dils@Dils-MacBook-Pro.local>
2021-02-17 20:14:31 -0800

    handle couple edge cases

--- beginning/js/01-larger-or-smaller.js
+++ beginning/js/01-larger-or-smaller.js
@@ -1,4 +1,6 @@
 let firstInt = Number(prompt('Enter the first integer'));
 let secondInt = Number(prompt('Enter the second integer'));
 
-firstInt > secondInt ? document.write(firstInt) : document.write(secondInt);
+isNaN(firstInt) === true || isNaN(secondInt) === true ? document.write('Please enter only integers '):
+firstInt === secondInt ? document.write('Both numbers are equal to each other: ', firstInt):
+firstInt > secondInt ? document.write("First integer is larger: ", firstInt) : document.write("Second integer is larger: ", secondInt);
