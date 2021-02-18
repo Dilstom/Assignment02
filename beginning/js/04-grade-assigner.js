a484b3f493beff5dc2b0151205aac1fd75997a7d
Dil Somb <dils@Dils-MacBook-Pro.local>
2021-02-17 20:02:56 -0800

    grade assigner

---
+++ beginning/js/04-grade-assigner.js
@@ -0,0 +1,24 @@
+// The “Grade Assigner” Application
+
+// Collect the a number between 1 and 100 from the user. 
+let selectedNum = Number(prompt('Enter a number between 1 and 100'));
+
+// console.log(isNaN(selectedNum))
+
+// only numbers between 1 and 100 are accepted
+if(selectedNum < 1 || selectedNum > 100 || isNaN(selectedNum) === true) {
+    document.write('Only numbers between 1 and 100 are accepted. Please refresh your browser to start the application again');
+}
+
+
+if(selectedNum >= 60 && selectedNum <= 69){
+    console.log('You received a D')
+}else if(selectedNum >= 70 && selectedNum <= 79) {
+    console.log('You received a C')
+}else if(selectedNum >= 80 && selectedNum <= 89) {
+    console.log('You received a B')
+}else if(selectedNum >= 90 && selectedNum <= 100) {
+    console.log('You received an A')
+} else {
+    console.log('You received an F')
+}
